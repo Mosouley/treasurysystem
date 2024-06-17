e3427372eb3871b91939ac4019403c2e9f67e64e
mosouley <mo.souley@gmail.com>
2024-06-17 19:21:42 +0300

    resetting git files

--- fxapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FxappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fxapp'
-
-    def ready(self):
-        import fxapp.signals
--- treasurysystem/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import re_path, include
-
-
-urlpatterns = [
-  re_path('admin/', admin.site.urls),
-  re_path('api/contacts/', include('contacts.urls')),
-  re_path('api/fx/', include('fxapp.urls')),
-]
